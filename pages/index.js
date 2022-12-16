d38b410d65a3640cee50e66adc1e672206e379e0
jaminur-islam <jaminurislam250@gmail.com>
2022-12-16 17:15:29 +0600

    link add

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
-
+import Link from "next/link"
 export default function HomePage(){
     return <div style={{background:"blue" , textAlign:"center" , fontSize: "30px", marginTop:"20px" , color:"white"}}> 
-        <a href="/about">
+     <Link href="/about">
+ 
             Click Pdf view 
-        </a>
+      
+     </Link>
+       
     </div>
 }
